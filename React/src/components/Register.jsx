469ced3647eb0fff010e2736d5836656c3956601
DamianMolter <163694119+DamianMolter@users.noreply.github.com>
2025-05-25 01:54:57 +0200

    Prepared Frontend for registration

--- React/src/components/Register.jsx
+++ React/src/components/Register.jsx
@@ -1,19 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Register(props) {
+
+async function registerUser(credentials) {
+ return fetch('http://localhost:4000/register', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json())
+}
+
+function Register({openLoginPage}) {
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const response = await registerUser({
+      email,
+      password,
+      confirmPassword
+    });
+    console.log(response);
+    const {token, userId, loginError} = response;
+    setToken(token);    
+    setLoggedUserId(userId);
+    setLoginError(loginError);
+  }
 
   return (
     <div className="registerContainer">
-      <div class="registerPanel">
+      <div className="registerPanel">
         <h1>Rejestracja</h1>
-        <form class="registerForm">
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Hasło" />
-          <input type="password" placeholder="Potwierdź hasło" />
+        <form className="registerForm" onSubmit={handleSubmit}>
+          <input type="email" placeholder="Email" onChange={event => {setEmail(event.target.value)}}/>
+          <input type="password" placeholder="Hasło" onChange={event => {setPassword(event.target.value)}}/>
+          <input type="password" placeholder="Potwierdź hasło" onChange={event => {setConfirmPassword(event.target.value)}}/>
           <button type="submit">Zarejestruj się</button>
-          <p class="login-register-text">
+          <p className="login-register-text">
             Masz już konto?{" "}
-            <a href="#" class="login-link" onClick={() => {props.openLoginPage(true)}}>
+            <a href="#" className="login-link" onClick={() => {openLoginPage(true)}}>
               Zaloguj się
             </a>
           </p>
